fdfc9d028dd0efaee0c0b1ddc1e113a77719c978
wingkwong <wingkwong.code@gmail.com>
2018-06-29 15:56:54 +0800

    revised index.jsx

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';  
+import rootReducer from './reducers';
+import Main from './Main';
 
-class MySuperCoolComponent extends Component {
-  handleClick = () => {
-    console.log("Click!");
-  };
+
+
+class VideoSlideSync extends Component {
+  constructor(props) {
+    super(props);
+
+  }
 
   render() {
-    const { color, children } = this.props;
+    const { player, context } = this.props;
+    const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);  
+    const store = createStoreWithMiddleware(rootReducer);
+
     return (
-      <button onClick={this.handleClick} style={{ color }}>
-        {children}
-      </button>
+      <Provider store={store}>
+        <Main player={player} context={context}/>
+      </Provider>
     );
   }
 }
 
-export default MySuperCoolComponent;
+
+export default VideoSlideSync;
